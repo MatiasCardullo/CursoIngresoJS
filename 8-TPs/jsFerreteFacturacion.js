0dbb487b8ad9c45b55e885a81318666423ddf451
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2018-02-13 15:52:21 -0300

    Practica 2

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,43 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+	
+	//1° creo las variables
+	var PrecioUno,PrecioDos,PrecioTres,Resultado
+
+	//2° tomo los datos insertados
+	//3° los identifico como numeros
+	//4° hago las operaciones aritmeticas y muestro el resultado
 function Sumar () 
 {
-	
+	PrecioUno=document.getElementById("PrecioUno").value;
+	PrecioDos=document.getElementById("PrecioDos").value;
+	PrecioTres=document.getElementById("PrecioTres").value;
+	PrecioUno=parseInt(PrecioUno);
+	PrecioDos=parseInt(PrecioDos);
+	PrecioTres=parseInt(PrecioTres);
+	Resultado=PrecioUno+PrecioDos+PrecioTres;
+	alert("Total: "+Resultado+"$");
 }
 function Promedio () 
 {
-	
+	PrecioUno=document.getElementById("PrecioUno").value;
+	PrecioDos=document.getElementById("PrecioDos").value;
+	PrecioTres=document.getElementById("PrecioTres").value;
+	PrecioUno=parseInt(PrecioUno);
+	PrecioDos=parseInt(PrecioDos);
+	PrecioTres=parseInt(PrecioTres);
+	Resultado=(PrecioUno+PrecioDos+PrecioTres)/3;
+	alert("Promedio: "+Resultado+"$");
 }
 function PrecioFinal () 
 {
-	
+	PrecioUno=document.getElementById("PrecioUno").value;
+	PrecioDos=document.getElementById("PrecioDos").value;
+	PrecioTres=document.getElementById("PrecioTres").value;
+	PrecioUno=parseInt(PrecioUno);
+	PrecioDos=parseInt(PrecioDos);
+	PrecioTres=parseInt(PrecioTres);
+	Resultado=(PrecioUno+PrecioDos+PrecioTres)*1.21;
+	alert("Total (IVA 21%): "+Resultado+"$");
 }
--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -2,12 +2,26 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
+	
+	//1° creo las variables
+	var Temperatura, resultado;
+
+	//2° tomo los datos insertados
+	//3° los identifico como numeros
+	//4° hago las operaciones aritmeticas y muestro el resultado
+	
 function FahrenheitCentigrados () 
 {
-	
+	Temperatura=document.getElementById("Temperatura").value;
+	Temperatura=parseInt(Temperatura);
+	resultado=(Temperatura-32)*(0.555)
+	alert(Temperatura+" Fahrenheit son "+resultado+" centígrados");
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	Temperatura=document.getElementById("Temperatura").value;
+	Temperatura=parseInt(Temperatura);
+	resultado=(1.8*Temperatura)+32
+	alert(Temperatura+" centígrados son "+resultado+" Fahrenheit")	
 }
