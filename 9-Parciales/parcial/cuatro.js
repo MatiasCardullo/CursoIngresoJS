4d6d4ce1ccd772c26f747cd8bb22901694eedeff
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2018-02-15 11:03:49 -0300

    Clase 3 practica

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,23 @@
 function Mostrar()
 {
-
+    var dato1,dato2,resultado;
+    dato1=prompt("Ingrese un numero","");
+    dato2=prompt("Ingrese otro numero","");
+    dato1=parseInt(dato1)
+    dato2=parseInt(dato2)
+    if(dato1==dato2)
+    {
+        resultado=dato1*dato2
+        document.write(resultado)
+    }
+    else if(dato1>dato2)
+    {
+        resultado=dato1-dato2
+        document.write(resultado)
+    }
+    else if(dato1<dato2)
+    {
+        resultado=dato1+dato2
+        document.write(resultado)
+    }
 }
