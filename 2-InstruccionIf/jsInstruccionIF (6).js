e3a5f0cd320388e577a4a04f65b321e901659ee9
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2018-02-15 11:02:43 -0300

    Clase 3
    if y else

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,18 @@
 function Mostrar()
 {
 //tomo la edad  
-
-
-
-
+var edad
+    edad=document.getElementById("edad").value
+    if(edad>=18)
+    {
+        alert("eres mayor de edad")
+    }
+    else if(edad>=13&&edad<=17)
+    {
+        alert("eres adolescente")
+    }
+    else if(edad<=12)
+    {
+        alert("eres niño")
+    }
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,11 @@
 function Mostrar()
 {
 //tomo la edad  
-
-	
-
-
+var edad,estadoCivil
+    edad=document.getElementById("edad").value
+    estadoCivil=document.getElementById("estadoCivil").value
+    if(edad>=18&&estadoCivil=="Soltero")
+    {
+        alert("Es soltero y no es menor")
+    }
 }//FIN DE LA FUNCIÓN
